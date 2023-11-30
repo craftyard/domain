767a0240bca7aa75528be9d545ee32b7199e6d7b
xHetso <datacener23491248@gmail.com>
2023-11-30 17:15:32 +0500

    Пофиксил ошибки

--- src/subject/domain-object/user/a-root.ts
+++ src/subject/domain-object/user/a-root.ts
@@ -6,14 +6,12 @@ import { Result } from 'rilata2/src/common/result/types';
 import { success } from 'rilata2/src/common/result/success';
 import * as jwt from 'jsonwebtoken';
 import { DomainResult } from 'rilata2/src/domain/domain-object-data/aggregate-data-types';
-import { LiteralFieldValidator } from 'rilata2/src/domain/validator/field-validator/literal-field-validator';
-import { ArrayFieldErrors, FieldErrors, ValidatorMap } from 'rilata2/src/domain/validator/field-validator/types';
+import { ArrayFieldErrors, FieldErrors } from 'rilata2/src/domain/validator/field-validator/types';
 import {
   AuthentificationUserActionParams,
   AuthentificationUserDomainQuery,
   JWTPayload,
   JwtTokens,
-  TelegramAuthDTO,
   TelegramDateNotValidError,
   TelegramHashNotValidError,
 } from '../../domain-data/user/user-authentification.a-params';
