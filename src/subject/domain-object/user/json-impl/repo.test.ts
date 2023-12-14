8c59bb2591faeeb8f83cdeb312c3fcb9f2e7e3cc
NurGeo <anzpro@gmail.com>
2023-12-14 17:07:27 +0500

    Реализован UserRepo.getUsers() вместе с тестами

--- src/subject/domain-object/user/json-impl/repo.test.ts
+++ src/subject/domain-object/user/json-impl/repo.test.ts
@@ -1,41 +1,7 @@
 import { describe, expect, test } from 'bun:test';
 import { ConsoleLogger } from 'rilata2/src/common/logger/console-logger';
-import { UserAttrs, UserProfile } from '../../domain-data/user/params';
-import { UserJsonRepository } from './json-repo';
-
-const testUsers: UserAttrs[] = [
-  {
-    userId: 'fa91a299-105b-4fb0-a056-92634249130c',
-    telegramId: 5436134100,
-    type: 'employee',
-    userProfile: {
-      firstName: 'Jack',
-      lastName: 'Smith',
-    },
-  },
-  {
-    userId: 'bc9166cb-ba37-43cb-93d3-ce6da27471df',
-    telegramId: 3290593910,
-    type: 'employee',
-    userProfile: {
-      firstName: 'Bill',
-      lastName: 'Oruell',
-    },
-  },
-  {
-    userId: 'bc9166cb-ba37-43cb-93d3-ce6da27471df',
-    telegramId: 5436134100,
-    type: 'client',
-    userProfile: {
-      firstName: 'Jack',
-      lastName: 'Smith',
-    },
-  },
-];
-
-const testUsersAsJson = JSON.stringify(testUsers);
-
-const getUserAttrs = (attrs: Partial<UserAttrs>) => JSON.stringify([{ ...testUsers[2], ...attrs }]);
+import { UserJsonRepository } from './repo';
+import { getUserAttrs, testUsersAsJson } from './fixture';
 
 describe('UserAr json implementation repository tests', () => {
   const logger = new ConsoleLogger();
