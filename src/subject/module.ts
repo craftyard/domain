c428efd25e5ca4e681cbe92a9eb8c51bcf93315c
NurGeo <anzpro@gmail.com>
2023-12-19 15:47:53 +0500

    SubjectModule переименован на SubjectDomainModule

--- src/subject/module.ts
+++ src/subject/module.ts
@@ -1,6 +1,6 @@
 import { Module } from 'rilata2/src/app/module/module';
 
-export abstract class SubjectModule extends Module<'module'> {
+export abstract class SubjectDomainModule extends Module<'module'> {
   moduleType: 'module' = 'module' as const;
 
   moduleName: string = 'subject';
