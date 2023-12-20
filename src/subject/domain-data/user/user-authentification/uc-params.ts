b4dd3ae08d8eef9b8050c7d6c988a9823a1616f7
NurGeo <anzpro@gmail.com>
2023-12-20 11:31:03 +0500

    Реализованы контракты UserAuthentificationUseCase

--- src/subject/domain-data/user/user-authentification/uc-params.ts
+++ src/subject/domain-data/user/user-authentification/uc-params.ts
@@ -1,20 +1,27 @@
-import { Caller } from 'rilata2/src/app/caller';
-import { UseCaseQueryDod } from 'rilata2/src/domain/domain-object-data/common-types';
-import { QueryUseCaseParams } from 'rilata2/src/app/use-case/types';
+import { ErrorDod } from 'rilata2/src/domain/domain-object-data/common-types';
+import { InputOptions, QueryUseCaseParams } from 'rilata2/src/app/use-case/types';
 import { JWTTokens } from 'rilata2/src/app/jwt/types';
-import { TelegramAuthDTO } from './a-params';
+import { AuthentificationUserActionParams, TelegramAuthDTO } from './a-params';
+import { UserParams } from '../params';
 
-export type AuthenticationUserUCQuery = UseCaseQueryDod<TelegramAuthDTO, 'AuthenticationUserUCQuery'>
-
-export type AuthenticationUserInputOptions = {
-  query: AuthenticationUserUCQuery,
-  caller: Caller;
+export type UserAuthentificationActionDod = {
+  actionName: 'userAuthentification',
+  body: TelegramAuthDTO,
 }
 
-export type AuthenticationUserSuccessOut = JWTTokens;
+export type UserAuthentificationInputOptions = InputOptions<UserAuthentificationActionDod>
 
-export type AuthenticationUserErrors = never;
+export type UserAuthentificationOut = JWTTokens;
+
+type UserIsAlreadyAuthorizedLocale = {
+  text: 'Вы уже авторизованы',
+  hint: Record<string, never>,
+}
+
+export type UserIsAlreadyAuthorizedError = ErrorDod<UserIsAlreadyAuthorizedLocale, 'UserIsAlreadyAuthorized'>
+
+export type UserAuthentificationErrors = UserIsAlreadyAuthorizedError | AuthentificationUserActionParams['errors'];
 
 export type AuthenticationUserUCParams = QueryUseCaseParams<
-  AuthenticationUserInputOptions, AuthenticationUserSuccessOut, AuthenticationUserErrors
+  UserParams, UserAuthentificationInputOptions, UserAuthentificationOut, UserAuthentificationErrors
 >
