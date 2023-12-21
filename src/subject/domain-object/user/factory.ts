c3645c46ff7af985d21a01298f9cf0dcf01db09c
NurGeo <anzpro@gmail.com>
2023-12-21 14:41:01 +0500

    подправлен UserArFactory

--- src/subject/domain-object/user/factory.ts
+++ src/subject/domain-object/user/factory.ts
@@ -1,7 +1,6 @@
 import { AggregateFactory } from 'rilata2/src/domain/domain-object/aggregate-factory';
 import { Logger } from 'rilata2/src/common/logger/logger';
 import { Caller } from 'rilata2/src/app/caller';
-import { AggregateRoot } from 'rilata2/src/domain/domain-object/aggregate-root';
 import { GeneralAR } from 'rilata2/src/domain/domain-object/types';
 import { UserAttrs, UserParams } from '../../domain-data/user/params';
 import { UserAR } from './a-root';
@@ -9,8 +8,8 @@ import { UserAR } from './a-root';
 export class UserFactory extends AggregateFactory<UserParams> {
   constructor(protected logger: Logger) { super(); }
 
-  create(caller: Caller, command: UserAttrs): AggregateRoot<UserParams> {
-    throw Error('should not be called until JSON implementation');
+  create(caller: Caller, attrs: UserAttrs): UserAR {
+    return new UserAR(attrs, 0, this.logger);
   }
 
   restore(userAttrs: UserAttrs, version: number): GeneralAR {
