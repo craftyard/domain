3566feab74f5e16c993c4f5db3ebb202da4fe90a
NurGeo <anzpro@gmail.com>
2023-12-23 10:08:14 +0500

    мелкие правки

--- src/subject/domain-object/user/json-impl/repo.ts
+++ src/subject/domain-object/user/json-impl/repo.ts
@@ -11,22 +11,22 @@ import { UserFactory } from '../factory';
 type UserRecord = UserAttrs & { version: number };
 
 export class UserJsonRepository implements UserReadRepository, UserCmdRepository {
-  private users: UserRecord[];
+  private usersRecords: UserRecord[];
 
   constructor(jsonUsers: string, protected logger: Logger) {
-    this.users = JSON.parse(jsonUsers);
+    this.usersRecords = JSON.parse(jsonUsers);
     const userVMap = new DtoFieldValidator('userMap', true, { isArray: true }, 'dto', userAttrsVMap);
-    const result = userVMap.validate(this.users);
+    const result = userVMap.validate(this.usersRecords);
     if (result.isFailure()) logger.error('Входящие данные не валидны', result.value);
   }
 
   async getUsers(userIds: string[]): Promise<UserAttrs[]> {
-    return this.users.filter((user) => userIds.includes(user.userId));
+    return this.usersRecords.filter((user) => userIds.includes(user.userId));
   }
 
   async findByTelegramId(telegramId: number): Promise<UserAR[]> {
-    const usersAttrs = this.users.filter((user) => user.telegramId === telegramId);
+    const usersRecords = this.usersRecords.filter((userRcd) => userRcd.telegramId === telegramId);
     const factory = new UserFactory(this.logger);
-    return usersAttrs.map((attrs) => factory.restore(dtoUtility.excludeAttrs(attrs, 'version'), attrs.version));
+    return usersRecords.map((rcd) => factory.restore(dtoUtility.excludeAttrs(rcd, 'version'), rcd.version));
   }
 }
