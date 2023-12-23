3556a174dac71e856d9050a8d299c64f403b2f01
NurGeo <anzpro@gmail.com>
2023-12-23 15:54:24 +0500

    Добавлены новые ошибки уровня user-auth-use-case

--- src/subject/domain-data/user/user-authentification/uc-params.ts
+++ src/subject/domain-data/user/user-authentification/uc-params.ts
@@ -1,7 +1,7 @@
 import { ErrorDod } from 'rilata2/src/domain/domain-object-data/common-types';
 import { InputOptions, QueryUseCaseParams } from 'rilata2/src/app/use-case/types';
 import { JWTTokens } from 'rilata2/src/app/jwt/types';
-import { AuthentificationUserActionParams, TelegramAuthDTO } from './a-params';
+import { UserAuthentificationActionParams, TelegramAuthDTO } from './a-params';
 import { UserParams } from '../params';
 
 export type UserAuthentificationActionDod = {
@@ -13,14 +13,23 @@ export type UserAuthentificationInputOptions = InputOptions<UserAuthentification
 
 export type UserAuthentificationOut = JWTTokens;
 
-type UserIsAlreadyAuthorizedLocale = {
-  text: 'Вы уже авторизованы',
-  hint: Record<string, never>,
+type ManyEmployeeAccountNotSupportedLocale = {
+  text: 'У вас с одним аккаунтом telegram имеется два пользовательских аккаунта сотрудников. К сожалению сейчас это не поддерживается. Обратитесь в техподдержку, чтобы вам помогли решить эту проблему.',
+  hint: { telegramId: number },
 }
 
-export type UserIsAlreadyAuthorizedError = ErrorDod<UserIsAlreadyAuthorizedLocale, 'UserIsAlreadyAuthorized'>
+export type ManyEmployeeAccountNotSupportedError = ErrorDod<ManyEmployeeAccountNotSupportedLocale, 'TwoEmployeeAccountNotSupportedError'>
 
-export type UserAuthentificationErrors = UserIsAlreadyAuthorizedError | AuthentificationUserActionParams['errors'];
+type EmployeeUserDoesNotExistLocale = {
+  text: 'У вас нет аккаунта сотрудника.',
+  hint: { telegramId: number },
+}
+
+export type EmployeeUserDoesNotExistError = ErrorDod<EmployeeUserDoesNotExistLocale, 'EmployeeUserDoesNotExistError'>
+
+export type UserAuthentificationErrors = ManyEmployeeAccountNotSupportedError
+  | EmployeeUserDoesNotExistError
+  | UserAuthentificationActionParams['errors'];
 
 export type UserAuthentificationUCParams = QueryUseCaseParams<
   UserParams, UserAuthentificationInputOptions, UserAuthentificationOut, UserAuthentificationErrors
