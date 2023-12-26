ab04eae8cf653456ceb3fbc2cc993cc1db75fcd5
FredSteady23 <fred.steady@mail.ru>
2023-12-26 16:14:39 +0500

    реализован resolver модуля Workshop

---
+++ src/workshop/resolver.ts
@@ -0,0 +1,27 @@
+import { Repositoriable } from 'rilata2/src/domain/repositoriable';
+import { Realisable } from 'rilata2/src/domain/realisable';
+import { Facadable } from 'rilata2/src/domain/facadable';
+import { Logger } from 'rilata2/src/common/logger/logger';
+import { WorkshopRepository } from '../workshop/domain-object/workshop/repository';
+
+export class SubjectResolver implements Repositoriable, Realisable, Facadable {
+  constructor(
+    protected workshopRepo: WorkshopRepository,
+    protected logger: Logger,
+  ) {}
+
+  getFacade(...args: unknown[]): unknown {
+    throw new Error('Method not implemented.');
+  }
+
+  getRealisation(key: unknown): unknown {
+    this.logger.error(`not finded key for getRealisation method of SubjectResolver, key: ${key}`);
+    throw Error();
+  }
+
+  getRepository(key: unknown): unknown {
+    if (key === WorkshopRepository) return this.workshopRepo;
+    this.logger.error(`not finded key for getRealisation method of SubjectResolver, key: ${key}`);
+    throw Error();
+  }
+}
