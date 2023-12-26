c82804af700f498280ecf28f3f2f5b2a3127db6e
xHetso <datacener23491248@gmail.com>
2023-12-26 17:56:59 +0500

    Реализовал use-case GetMyWorkshop

--- src/workshop/domain-data/workshop/find-workshop-by-user-id/uc-vmap.ts
+++ src/workshop/domain-data/workshop/find-workshop-by-user-id/uc-vmap.ts
@@ -1,6 +1,9 @@
 /* eslint-disable function-paren-newline */
+import { ActionDodValidator } from 'rilata2/src/app/use-case/types';
 import { DtoFieldValidator } from 'rilata2/src/domain/validator/field-validator/dto-field-validator';
+import { GetMyWorkshopUcParams } from './uc-params';
 
-export const getMyWorkshopValidator = new DtoFieldValidator(
+export const getMyWorkshopValidator:
+ActionDodValidator<GetMyWorkshopUcParams> = new DtoFieldValidator(
   'getMyWorkshop', true, { isArray: false }, 'dto', {},
 );
