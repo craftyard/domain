8952e7eb3bb8a01c46485adf70f5ee02982ed571
NurGeo <anzpro@gmail.com>
2023-12-28 02:01:09 +0500

    подправлены user-factory, workshop-factory, метод create() помечен как нереализованный

--- src/subject/domain-object/user/factory.ts
+++ src/subject/domain-object/user/factory.ts
@@ -1,11 +1,13 @@
 import { AggregateFactory } from 'rilata/src/domain/domain-object/aggregate-factory';
+import { AssertionException } from 'rilata/src/common/exeptions';
 import { Caller } from 'rilata/src/app/caller';
 import { UserAttrs, UserParams } from '../../domain-data/user/params';
 import { UserAR } from './a-root';
 
 export class UserFactory extends AggregateFactory<UserParams> {
-  create(caller: Caller, attrs: UserAttrs): UserAR {
-    return new UserAR(attrs, 0, this.logger);
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  create(caller: Caller, action: unknown): UserAR {
+    throw new AssertionException('not implemented');
   }
 
   restore(userAttrs: UserAttrs, version: number): UserAR {
