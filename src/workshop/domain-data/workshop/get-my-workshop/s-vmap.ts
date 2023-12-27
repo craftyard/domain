8f7ebfde31fbb83cb2b24870983baf53cd121213
xHetso <datacener23491248@gmail.com>
2023-12-27 11:29:31 +0500

    Пофиксил ошибки

--- src/workshop/domain-data/workshop/get-my-workshop/s-vmap.ts
+++ src/workshop/domain-data/workshop/get-my-workshop/s-vmap.ts
@@ -1,9 +1,9 @@
 /* eslint-disable function-paren-newline */
 import { ActionDodValidator } from 'rilata2/src/app/service/types';
 import { DtoFieldValidator } from 'rilata2/src/domain/validator/field-validator/dto-field-validator';
-import { GetMyWorkshopUcParams } from './uc-params';
+import { GetMyWorkshopServiceParams } from './s-params';
 
 export const getMyWorkshopValidator:
-ActionDodValidator<GetMyWorkshopUcParams> = new DtoFieldValidator(
+ActionDodValidator<GetMyWorkshopServiceParams> = new DtoFieldValidator(
   'getMyWorkshop', true, { isArray: false }, 'dto', {},
 );
