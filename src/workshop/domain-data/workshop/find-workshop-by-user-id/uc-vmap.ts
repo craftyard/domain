51b68332964779839a92c52f3648dcc5bd0c378d
xHetso <datacener23491248@gmail.com>
2023-12-27 11:18:28 +0500

    Отрефакторил uc-vmap

--- src/workshop/domain-data/workshop/find-workshop-by-user-id/uc-vmap.ts
+++ src/workshop/domain-data/workshop/find-workshop-by-user-id/uc-vmap.ts
@@ -1,5 +1,5 @@
 /* eslint-disable function-paren-newline */
-import { ActionDodValidator } from 'rilata2/src/app/use-case/types';
+import { ActionDodValidator } from 'rilata2/src/app/service/types';
 import { DtoFieldValidator } from 'rilata2/src/domain/validator/field-validator/dto-field-validator';
 import { GetMyWorkshopUcParams } from './uc-params';
 
