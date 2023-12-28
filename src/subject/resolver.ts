482e01984c0ddca855c0aa128bed840acccdffe4
Nuradil <yaksh0nti@gmail.com>
2023-12-29 03:14:42 +0500

    теперь SubjectResolver имплементирует интерфейс ModuleResolver

--- src/subject/resolver.ts
+++ src/subject/resolver.ts
@@ -1,14 +1,18 @@
 import { Logger } from 'rilata/src/common/logger/logger';
 import { TokenCreator } from 'rilata/src/app/jwt/token-creator.interface';
 import { JWTTokens } from 'rilata/src/app/jwt/types';
-import { Repositoriable } from 'rilata/src/app/resolves/repositoriable';
-import { Realisable } from 'rilata/src/app/resolves/realisable';
-import { Facadable } from 'rilata/src/app/resolves/facadable';
+import { ModuleResolver } from 'rilata/src/app/resolves/module-resolver';
 import { AssertionException } from 'rilata/src/common/exeptions';
-import { UserReadRepository } from './domain-object/user/read-repository';
+import { Database } from 'rilata/src/app/database/database';
+import { TokenVerifier } from 'rilata/src/app/jwt/token-verifier.interface';
+import { Module } from 'rilata/src/app/module/module';
 import { UserCmdRepository } from './domain-object/user/cmd-repository';
+import { UserReadRepository } from './domain-object/user/read-repository';
+import { JWTPayload } from './domain-data/user/user-authentification/a-params';
 
-export class SubjectResolver implements Repositoriable, Realisable, Facadable {
+export class SubjectResolver implements ModuleResolver {
+  private module!: Module;
+
   constructor(
     protected botToken: string,
     protected tokenCreator: TokenCreator<JWTTokens>,
@@ -16,6 +20,26 @@ export class SubjectResolver implements Repositoriable, Realisable, Facadable {
     protected userCmdRepo: UserCmdRepository,
     protected logger: Logger,
   ) {}
+
+  init(module: Module): void {
+    this.module = module;
+  }
+
+  getTokenVerifier(): TokenVerifier<JWTPayload> {
+    throw new Error('Method not implemented.');
+  }
+
+  getModule(): Module {
+    return this.module;
+  }
+
+  getLogger(): Logger {
+    return this.logger;
+  }
+
+  getDatabase(): Database {
+    throw new Error('Method not implemented.');
+  }
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   getFacade(...args: unknown[]): unknown {
