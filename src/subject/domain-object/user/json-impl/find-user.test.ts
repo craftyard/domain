dcd028caa4f4447923c3b536ae347b2f08ac0206
xHetso <datacener23491248@gmail.com>
2024-01-10 16:11:43 +0500

    Реализовал репозиторий для use-case getUser

---
+++ src/subject/domain-object/user/json-impl/find-user.test.ts
@@ -0,0 +1,39 @@
+import { describe, expect, test } from 'bun:test';
+import { ConsoleLogger } from 'rilata/src/common/logger/console-logger';
+import { UserJsonRepository } from './repo';
+import { testUsersRecordsAsJson } from './fixture';
+
+describe('Тесты для получения user-a по userId', () => {
+  const logger = new ConsoleLogger();
+
+  const sut = new UserJsonRepository(testUsersRecordsAsJson, logger);
+  test('Успех, получен обьект мастерской', async () => {
+    const user = await sut.getUser('fa91a299-105b-4fb0-a056-92634249130c');
+    expect(user.value).toEqual({
+      userId: 'fa91a299-105b-4fb0-a056-92634249130c',
+      telegramId: 5436134100,
+      type: 'employee',
+      userProfile: {
+        firstName: 'Jack',
+        lastName: 'Smith',
+      },
+    });
+  });
+  test('Провал, пользователь по такому id не найден', async () => {
+    const userId = '3332a8d6-67ab-4e87-8a21-9d17f508fd5c';
+    const user = await sut.getUser(userId);
+    expect(user.value).toEqual({
+      locale: {
+        text: 'Такого пользователя не существует',
+        hint: {
+          userId: '3332a8d6-67ab-4e87-8a21-9d17f508fd5c',
+        },
+      },
+      meta: {
+        name: 'UserDoesNotExistError',
+        errorType: 'domain-error',
+        domainType: 'error',
+      },
+    });
+  });
+});
--- src/subject/domain-object/user/json-impl/repo.ts
+++ src/subject/domain-object/user/json-impl/repo.ts
@@ -2,12 +2,18 @@ import { dtoUtility } from 'rilata/src/common/utils/dto/';
 import { Logger } from 'rilata/src/common/logger/logger';
 import { DtoFieldValidator } from 'rilata/src/domain/validator/field-validator/dto-field-validator';
 import { AssertionException } from 'rilata/src/common/exeptions';
+import { UserId } from 'rilata/src/common/types';
+import { Result } from 'rilata/src/common/result/types';
+import { dodUtility } from 'rilata/src/common/utils/domain-object/dod-utility';
+import { failure } from 'rilata/src/common/result/failure';
+import { success } from 'rilata/src/common/result/success';
 import { UserAttrs } from '../../../domain-data/user/params';
 import { userAttrsVMap } from '../../../domain-data/user/v-map';
 import { UserReadRepository } from '../read-repository';
 import { UserCmdRepository } from '../cmd-repository';
 import { UserAR } from '../a-root';
 import { UserFactory } from '../factory';
+import { UserDoesNotExistError } from '../../../domain-data/user/get-user/s-params';
 
 type UserRecord = UserAttrs & { version: number };
 
@@ -29,6 +35,20 @@ export class UserJsonRepository implements UserReadRepository, UserCmdRepository
     return this.usersRecords
       .filter((records) => userIds.includes(records.userId))
       .map((records) => dtoUtility.excludeAttrs(records, 'version'));
+  }
+
+  async getUser(userId: UserId): Promise <Result<UserDoesNotExistError, UserAttrs>> {
+    const foundUser = this.usersRecords
+      .find((user) => user.userId.includes(userId));
+    if (foundUser) {
+      const user: UserAttrs = dtoUtility.excludeAttrs(foundUser, 'version');
+      return success(user);
+    }
+    return failure(dodUtility.getDomainErrorByType<UserDoesNotExistError>(
+      'UserDoesNotExistError',
+      'Такого пользователя не существует',
+      { userId },
+    ));
   }
 
   async findByTelegramId(telegramId: number): Promise<UserAR[]> {
