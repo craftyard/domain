55370f730e71e8f45c9cf607c8dae64a6b9d74eb
xHetso <datacener23491248@gmail.com>
2024-02-08 17:55:02 +0500

    Пофиксил ошибки

--- src/workshop/domain-data/workshop/find-workshop-by-user-id/s-vmap.ts
+++ src/workshop/domain-data/workshop/find-workshop-by-user-id/s-vmap.ts
@@ -3,19 +3,11 @@ import { ActionDodValidator, GetActionDodBody } from 'rilata/src/app/service/typ
 import { DtoFieldValidator } from 'rilata/src/domain/validator/field-validator/dto-field-validator';
 import { FindWorkshopByUserIdServiceParams } from './s-params';
 import { ValidatorMap } from 'rilata/src/domain/validator/field-validator/types';
-import { LiteralFieldValidator } from 'rilata/src/domain/validator/field-validator/literal-field-validator';
-import { UUIDFormatValidationRule } from 'rilata/src/domain/validator/rules/validate-rules/string/uuid-format.v-rule';
-
+import { userAttrsVMap } from '../../../../subject/domain-data/user/v-map';
 
 const gettingUserVMap: ValidatorMap<GetActionDodBody<FindWorkshopByUserIdServiceParams>> = {
-  userId: new LiteralFieldValidator(
-    'userId',
-    true,
-    { isArray: false },
-    'string',
-    [new UUIDFormatValidationRule()],
-  ),
-};
+  userId: userAttrsVMap.userId,
+}
 
 export const findWorkshopByUserIdValidator:
 ActionDodValidator<FindWorkshopByUserIdServiceParams> = new DtoFieldValidator(
