e63ab739cec6bd72e4e2e9f3d1133cd5e8a68725
Hetso <datacener23491248@gmail.com>
2024-02-12 16:09:38 +0500

    Пофиксил ошибки

--- src/workshop/domain-data/workshop/find-workshop-by-user-id/s-vmap.ts
+++ src/workshop/domain-data/workshop/find-workshop-by-user-id/s-vmap.ts
@@ -1,13 +1,13 @@
 /* eslint-disable function-paren-newline */
 import { ActionDodValidator, GetActionDodBody } from 'rilata/src/app/service/types';
 import { DtoFieldValidator } from 'rilata/src/domain/validator/field-validator/dto-field-validator';
-import { FindWorkshopByUserIdServiceParams } from './s-params';
 import { ValidatorMap } from 'rilata/src/domain/validator/field-validator/types';
+import { FindWorkshopByUserIdServiceParams } from './s-params';
 import { userAttrsVMap } from '../../../../subject/domain-data/user/v-map';
 
 const gettingUserVMap: ValidatorMap<GetActionDodBody<FindWorkshopByUserIdServiceParams>> = {
   userId: userAttrsVMap.userId,
-}
+};
 
 export const findWorkshopByUserIdValidator:
 ActionDodValidator<FindWorkshopByUserIdServiceParams> = new DtoFieldValidator(
