cf835a402da24488a6b0e66e03a066ee219e5182
Renatter <riko321riko321@gmail.com>
2024-02-12 16:56:24 +0600

    feature:создал контракты для getCurrentUser

---
+++ src/subject/domain-data/user/get-current-user/s-params.ts
@@ -0,0 +1,21 @@
+import { QueryServiceParams } from 'rilata/src/app/service/types';
+import { UuidType } from 'rilata/src/common/types';
+import { UserAttrs, UserParams } from '../params';
+import { WorkshopAttrs } from '../../../../workshop/domain-data/workshop/params';
+
+export type GetCurrentUserActionDod = {
+  meta: {
+    name: 'GetCurrentUser',
+    actionId: UuidType,
+    domainType: 'action',
+  },
+attrs: Record<string, never>,
+}
+export type CurrentUser = UserAttrs & {
+    workshopName: WorkshopAttrs['name'];
+};
+export type GetingCurrentUserOut = CurrentUser
+
+export type GetingUserServiceParams = QueryServiceParams<
+  UserParams, GetCurrentUserActionDod, GetingCurrentUserOut, never
+>
