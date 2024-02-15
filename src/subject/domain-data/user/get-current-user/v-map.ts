6e5464f81238d62656526ec57d532de5f2355c11
Hetso <datacener23491248@gmail.com>
2024-02-15 15:51:19 +0500

    Пофиксил ошибки

--- src/subject/domain-data/user/get-current-user/v-map.ts
+++ src/subject/domain-data/user/get-current-user/v-map.ts
@@ -3,7 +3,7 @@ import { ActionDodValidator } from 'rilata/src/app/service/types';
 import { DtoFieldValidator } from 'rilata/src/domain/validator/field-validator/dto-field-validator';
 import { GetingCurrentUserServiceParams } from './s-params';
 
-export const getCurrentUserServiceValidator:
+export const getCurrentUserValidator:
 ActionDodValidator<GetingCurrentUserServiceParams> = new DtoFieldValidator(
   'getCurrentUser', true, { isArray: false }, 'dto', {},
 );
