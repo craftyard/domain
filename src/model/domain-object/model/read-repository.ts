c410528e6a71cc4f314a919570b891cc7c634eb2
Hetso <datacener23491248@gmail.com>
2024-03-04 17:04:50 +0500

    fixed errors

--- src/model/domain-object/model/read-repository.ts
+++ src/model/domain-object/model/read-repository.ts
@@ -1,12 +1,14 @@
 import { Repositoriable } from 'rilata/src/app/resolves/repositoriable';
+import { ServiceResult } from 'rilata/src/app/service/types';
 import { UuidType } from 'rilata/src/common/types';
+import { GettingWorkshopModelServiceParams } from '../../domain-data/model/get-model/s-params';
 import { ModelAttrs } from '../../domain-data/params';
 
 export interface ModelReadRepository {
     getWorkshopModels(workshopId:UuidType): Promise<ModelAttrs[]>;
 
-    findWorkshopModel(workshopId: UuidType, modelId: UuidType):
-    Promise<ModelAttrs | undefined>;
+    getWorkshopModel(workshopId: UuidType, modelId: UuidType):
+    Promise<ServiceResult<GettingWorkshopModelServiceParams>>;
 }
 
 export const ModelReadRepository = {
