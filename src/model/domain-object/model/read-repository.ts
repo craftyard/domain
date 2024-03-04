ede99d2b5b66eecd6e2651eee2b470fa15881c1a
Hetso <datacener23491248@gmail.com>
2024-03-04 15:47:26 +0500

    Refactor getWorkshopModel to findWorkshopModel

--- src/model/domain-object/model/read-repository.ts
+++ src/model/domain-object/model/read-repository.ts
@@ -7,8 +7,8 @@ import { ModelAttrs } from '../../domain-data/params';
 export interface ModelReadRepository {
     getWorkshopModels(workshopId:UuidType): Promise<ModelAttrs[]>;
 
-    getWorkshopModel(workshopId: UuidType, modelId: UuidType):
-    Promise<ServiceResult<GettingWorkshopModelServiceParams>>;
+    findWorkshopModel(workshopId: UuidType, modelId: UuidType):
+    Promise<ModelAttrs | undefined>;
 }
 
 export const ModelReadRepository = {
