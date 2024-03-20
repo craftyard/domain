8635766564ec80a179e70e372c787b362a47e707
Hetso <datacener23491248@gmail.com>
2024-03-20 12:22:54 +0500

    Fix import path in UserReadRepository

--- src/subject/domain-object/user/read-repository.ts
+++ src/subject/domain-object/user/read-repository.ts
@@ -2,9 +2,10 @@ import { Repositoriable } from 'rilata/src/app/resolves/repositoriable';
 import { UserId } from 'rilata/src/common/types';
 import { Result } from 'rilata/src/common/result/types';
 import { UserAttrs } from '../../domain-data/user/params';
-import { UserDoesNotExistError } from '../../domain-data/user/get-user/s-params';
+
 import { UserAR } from './a-root';
 import { TelegramId } from '../../../types';
+import { UserDoesNotExistError } from '../../service/user/get-user/s-params';
 
 export interface UserReadRepository {
   init(resolver: Repositoriable): void
